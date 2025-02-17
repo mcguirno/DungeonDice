3af5aee3c240f7440cf6ec658e7e7328d1fc567f
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-17 13:58:02 -0500

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Noah McGuire on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
